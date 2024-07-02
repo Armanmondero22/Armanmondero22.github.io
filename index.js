f5781d10275ae70f6936345e1257ab0fdd073dbb
Armanmondero22 <armanmondero352@gmail.com>
2024-07-02 19:39:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -34,38 +34,40 @@ function addOrder() {
 
     if (parseFloat(qty1.value) > 0) {
         var order1 = qty1.value + " pc/s x " + price1.textContent + " - " + product1.textContent + " - Php " + (parseFloat(qty1.value) * parseFloat(price1.textContent)).toFixed(2) + "\n";
-        carts.textContent +=order
+        orders += order1;
         totalAmount += parseFloat(qty1.value) * parseFloat(price1.textContent);
     }
 
     if (parseFloat(qty2.value) > 0) {
         var order2 = qty2.value + " pc/s x " + price2.textContent + " - " + product2.textContent + " - Php " + (parseFloat(qty2.value) * parseFloat(price2.textContent)).toFixed(2) + "\n";
-        orders += ;
+        orders += order2;
         totalAmount += parseFloat(qty2.value) * parseFloat(price2.textContent);
     }
 
     if (parseFloat(qty3.value) > 0) {
         var order3 = qty3.value + " pc/s x " + price3.textContent + " - " + product3.textContent + " - Php " + (parseFloat(qty3.value) * parseFloat(price3.textContent)).toFixed(2) + "\n";
-        orders += ;
+        orders += order3;
         totalAmount += parseFloat(qty3.value) * parseFloat(price3.textContent);
     }
 
     if (parseFloat(qty4.value) > 0) {
         var order4 = qty4.value + " pc/s x " + price4.textContent + " - " + product4.textContent + " - Php " + (parseFloat(qty4.value) * parseFloat(price4.textContent)).toFixed(2) + "\n";
-        orders += ;
+        orders += order4;
         totalAmount += parseFloat(qty4.value) * parseFloat(price4.textContent);
     }
+
     if (parseFloat(qty5.value) > 0) {
         var order5 = qty5.value + " pc/s x " + price5.textContent + " - " + product5.textContent + " - Php " + (parseFloat(qty5.value) * parseFloat(price5.textContent)).toFixed(2) + "\n";
-        orders += ;
+        orders += order5;
         totalAmount += parseFloat(qty5.value) * parseFloat(price5.textContent);
     }
 
     if (parseFloat(qty6.value) > 0) {
         var order6 = qty6.value + " pc/s x " + price6.textContent + " - " + product6.textContent + " - Php " + (parseFloat(qty6.value) * parseFloat(price6.textContent)).toFixed(2) + "\n";
-        orders += ;
+        orders += order6;
         totalAmount += parseFloat(qty6.value) * parseFloat(price6.textContent);
     }
+
     carts.textContent = orders;
     total.value = "Php " + totalAmount.toFixed(2);
 }
@@ -85,7 +87,7 @@ function finishOrder() {
     var cashAmount = parseFloat(cash.value);
 
     if (cashAmount >= totalAmount && totalAmount > 0) {
-        alert("Order Successful!");
+        alert("Order Successful!\n\n" + "Products Purchased:\n" + carts.textContent + "\nTHANK YOU !");
         carts.textContent = "";
         total.value = "";
         cash.value = "";
@@ -96,7 +98,6 @@ function finishOrder() {
         qty4.value = "";
         qty5.value = "";
         qty6.value = "";
-          }
     } else if (totalAmount == 0) {
         alert("Please add items to your order.");
     } else {
